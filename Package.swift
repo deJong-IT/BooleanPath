eeda0df44c49d71a7d2bd397b13998e07555bcb4
Jory de Kort <jory@jorydekort.nl>
2022-03-22 09:33:11 +0100

    fixed path to files

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "BooleanPath",
-            dependencies: []),
+            dependencies: [],
+			path: "./BooleanPath/"),
         .testTarget(
             name: "BooleanPathTests",
             dependencies: ["BooleanPath"]),
